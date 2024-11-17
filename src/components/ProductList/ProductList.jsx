3d948f5ea107afaaaf7881b221a5f04e73722ec8
43m04ka <89536403007r@gmail.com>
2024-11-18 01:35:45 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('https://45.89.24.164:8000/web-data', {
+        fetch('http://45.89.24.164:8000/web-data/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',

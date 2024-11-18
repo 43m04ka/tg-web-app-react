cb82779d83511528465de4a8671d5d753836c8b9
43m04ka <89536403007r@gmail.com>
2024-11-18 23:10:42 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 
+const express = require('express');
+
+const PORT = 17405;
+
+const app = express();
+
+app.get('/', (req, res) => {res.send('123')})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
--- src/postgress/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const PORT = 17405;
-
-const app = express();
-
-app.get('/', (req, res) => {res.send('123')})
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

2c325e0728949b51f320b6ecaf9d003e074118f5
43m04ka <89536403007r@gmail.com>
2024-12-05 01:25:00 +0300

    init

--- src/components/UI/ProductItem.jsx
+++ src/components/UI/ProductItem.jsx
@@ -21,7 +21,6 @@ const ProductItem = ({product, className, onAdd}) => {
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
             </Button>
-
         </div>
     );
 };

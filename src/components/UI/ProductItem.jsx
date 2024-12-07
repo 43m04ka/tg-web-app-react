145b69e7fa3b9341a5009b3de5bc59feb0cd5e99
43m04ka <89536403007r@gmail.com>
2024-12-08 01:17:13 +0300

    init

--- src/components/UI/ProductItem.jsx
+++ src/components/UI/ProductItem.jsx
@@ -7,10 +7,11 @@ const ProductItem = ({product, className, onAdd}) => {
     const onAddHandler = () => {
         onAdd(product);
     }
+    console.log(product);
 
     return (
         <div className={'list-element'}>
-            <Link to={'/home/' + product.path + '/' + product.id} className={'link-element'}>
+            <Link to={'/home/' + product.category + '/' + product.id} className={'link-element'}>
                 <div className={'box-home-block-element'}>
                     <img src={product.img} alt={product.title} className={'img-home'}/>
                     <div className={'text-element name-element'}>{product.title}</div>

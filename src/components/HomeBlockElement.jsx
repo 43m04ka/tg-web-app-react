b7402fd9cdb0b34456bf3d9bfe28d010d0417413
43m04ka <89536403007r@gmail.com>
2024-12-07 21:58:44 +0300

    init

--- src/components/HomeBlockElement.jsx
+++ src/components/HomeBlockElement.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 const HomeBlockElement = ({path, data}) => {
     return (
         <div className={'home-block-element'}>
-            <Link to={'/home/' + path + '/' + data.id}>
+            <Link to={'/home/' + path + '/' + data.id} className={'link-element'}>
                 <div className={'box-home-block-element'}>
                     <img src={data.img} alt={data.title} className={'img-home'}/>
                     <div className={'name-block'}>{data.title}</div>

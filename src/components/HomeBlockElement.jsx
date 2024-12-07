3dff2bb8dd9e4dc14d3b80da981c3ad1ccca7897
43m04ka <89536403007r@gmail.com>
2024-12-08 00:01:40 +0300

    init

--- src/components/HomeBlockElement.jsx
+++ src/components/HomeBlockElement.jsx
@@ -7,7 +7,7 @@ const HomeBlockElement = ({path, data}) => {
             <Link to={'/home/' + path + '/' + data.id} className={'link-element'}>
                 <div className={'box-home-block-element'}>
                     <img src={data.img} alt={data.title} className={'img-home'}/>
-                    <div className={'text-element'}>{data.title}</div>
+                    <div className={'text-element name-element'}>{data.title}</div>
                     <div className={'text-element price-element'}>{String(data.price) + ' ₽'}</div>
                 </div>
             </Link>

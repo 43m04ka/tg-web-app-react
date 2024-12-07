afbb1218a2f5f4031168060bbe14935ae5599291
43m04ka <89536403007r@gmail.com>
2024-12-08 00:15:27 +0300

    init

--- src/components/UI/ProductItem.jsx
+++ src/components/UI/ProductItem.jsx
@@ -8,33 +8,15 @@ const ProductItem = ({product, className, onAdd}) => {
         onAdd(product);
     }
 
-    const [hovered, setHovered] = useState(false);
-
-    const boxStyle = {
-        backgroundColor: hovered ? 'rgb(74,76,81)' : '#1e1f22',
-        transition: 'background-color 0.5s ease',
-    }
-
     return (
-        <div style={{padding: '5px'}}>
-            <div className={'product ' + className}>
-
-                <Link to={String(product.id)}>
-                    <div className={''}>{product.title}</div>
-                    <div className={''}>{product.price}</div>
-                    <div className={'img-box'}>
-                        <img src={product.img} className={'img'} alt="Product Image"/>
-                    </div>
-
-                </Link>
-                {/*<button className={'add-btn'}*/}
-                {/*        onClick={onAddHandler}*/}
-                {/*        style={boxStyle}*/}
-                {/*        onMouseEnter={() => setHovered(true)}*/}
-                {/*        onMouseLeave={() => setHovered(false)}>*/}
-                {/*    Добавить в корзину*/}
-                {/*</button>*/}
-            </div>
+        <div className={'list-element'}>
+            <Link to={'/home/' + product.path + '/' + product.id} className={'link-element'}>
+                <div className={'box-home-block-element'}>
+                    <img src={product.img} alt={product.title} className={'img-home'}/>
+                    <div className={'text-element name-element'}>{product.title}</div>
+                    <div className={'text-element price-element'}>{String(product.price) + ' ₽'}</div>
+                </div>
+            </Link>
         </div>
     )
         ;

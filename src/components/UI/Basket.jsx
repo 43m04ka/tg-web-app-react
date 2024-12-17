e4cb3a62be0c9c157e736e9d81c2966a36905800
43m04ka <89536403007r@gmail.com>
2024-12-17 23:54:20 +0300

    init

--- src/components/UI/Basket.jsx
+++ src/components/UI/Basket.jsx
@@ -12,6 +12,25 @@ const Basket = () => {
 
     const [basket, setBasket] = useState([])
 
+    const sendDataProduct = {
+        method: 'buy',
+        user: {id: 5106439090, first_name: "tёma"},
+    }
+
+    const onClickButton = useCallback(() => {
+        fetch('https://2ae04a56-b56e-4cc1-b14a-e7bf1761ebd5.selcdn.net/basket', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(sendDataProduct)
+        }).then(r => {console.log(r)})
+    }, [sendDataProduct])
+
+    useEffect(() => {
+        onGetData()
+    }, []);
+
     const sendData = {
         method: 'get',
         user: {id: 5106439090, first_name: "tёma"},
@@ -38,10 +57,6 @@ const Basket = () => {
         })
     }, [sendData])
 
-    useEffect(() => {
-        onGetData()
-    }, []);
-
     const onBack = useCallback(async () => {
         navigate(-1);
     }, [])
@@ -57,6 +72,11 @@ const Basket = () => {
         tg.BackButton.show();
     }, [])
 
+    let sumPrice = 0
+
+    basket.map(el => {
+        return  sumPrice += el.price
+    })
 
     if (isResizeble) {
         return (<div className={'pong-loader'} style={{
@@ -66,18 +86,40 @@ const Basket = () => {
         }}>Ожидайте</div>);
     } else if (basket.length === 0) {
         return (
-            <div>
-                <div className={'title'} style={{marginLeft:String(window.innerWidth/2-60)+'px'}}>Корзина пуста</div>
-                <Link to={'/home'} className={'link-element'}>
-                <button className={'all-see-button'}>За покупками</button>
-                </Link>
+            <div style={{display: 'grid', height: '100%'}}>
+                <div style={{
+                    height: String(window.innerHeight - 15 - 100) + 'px',
+                    marginTop: '15px', marginLeft : String(window.innerWidth/2-45)+'px',
+                    color:'gray'
+                }} className={'text-element'}>
+                    Корзина пуста
+                </div>
+                <div style={{marginBottom: '0px', position: 'relative'}}>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        width: '100%',
+                        justifyContent: 'space-between',
+                        borderTop: '2px solid gray'
+                    }}>
+                        <div style={{marginTop: '10px', fontSize: '20px', marginLeft: '20px'}}
+                             className={'text-element'}>Итого:
+                        </div>
+                        <div style={{marginTop: '10px', fontSize: '20px', marginRight: '20px'}}
+                             className={'text-element'}>0 ₽
+                        </div>
+                    </div>
+                    <Link to={'/home'} className={'link-element'}>
+                        <button className={'all-see-button'} style={{marginTop: '10px'}}>На главную</button>
+                    </Link>
+                </div>
             </div>)
     } else {
         return (
             <div style={{display: 'grid', height: '100%'}}>
                 <div style={{
                     height: String(window.innerHeight - 15 - 100) + 'px',
-                    marginTop: '15px'
+                    marginTop: '15px', overflowY:'scroll'
                 }}>
                     {basket.map(el => (
                         <ProductItemBasket key={el.id} setBasketF={setBasket} product={el}/>
@@ -95,10 +137,10 @@ const Basket = () => {
                              className={'text-element'}>Итого:
                         </div>
                         <div style={{marginTop: '10px', fontSize: '20px', marginRight: '20px'}}
-                             className={'text-element'}>{String(123)} ₽
+                             className={'text-element'}>{String(sumPrice)} ₽
                         </div>
                     </div>
-                    <button className={'all-see-button'} style={{marginTop: '10px'}}>Оформить заказ</button>
+                    <button className={'all-see-button'} style={{marginTop: '10px'}} onClick={onClickButton}>Оформить заказ</button>
                 </div>
             </div>
 

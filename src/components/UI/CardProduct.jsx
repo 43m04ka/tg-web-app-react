0f7466e6afd87dd25dc33f04720e72f8e24432cd
43m04ka <89536403007r@gmail.com>
2024-12-28 01:16:55 +0300

    init

--- src/components/UI/CardProduct.jsx
+++ src/components/UI/CardProduct.jsx
@@ -5,6 +5,8 @@ import {useNavigate, useParams} from "react-router-dom";
 const CardProduct = ({mainData}) => {
     const {tg, user} = useTelegram();
     const navigate = useNavigate();
+    const [isBuy, setIsBuy] = React.useState(false);
+    const [buttonText, setButtonText] = React.useState('Добавить в корзину');
 
     const onBack = useCallback(async () => {
         navigate(-1);
@@ -17,12 +19,16 @@ const CardProduct = ({mainData}) => {
         }
     }, [onBack])
 
+    const onBasket = useCallback(async () => {
+        navigate('/home/basket');
+    }, [])
+
     useEffect(() => {
         tg.BackButton.show();
     }, [])
 
     const sendData = {
-        method:'add',
+        method: 'add',
         mainData: mainData,
         user: user,
     }
@@ -34,16 +40,78 @@ const CardProduct = ({mainData}) => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(sendData)
-        }).then(r => console.log(r))
+        }).then(r => {
+            let Promise = r.json()
+            Promise.then(prom => {
+                const result = prom.body
+                if(result) {
+                    setIsBuy(true)
+                    setButtonText('Перейти в корзину')
+                }else{setButtonText('Добавить в корзину')}
+            })
+        })
     }, [sendData])
+
+    let buttonColor = '#51a456'
+    let buttonLink = () => {onSendData();setButtonText('Ожидайте...');}
+    if(isBuy) {
+        buttonColor = '#414BE0FF'
+        buttonLink = () => {onBasket()}
+    }
+
+    let descriptionText = ''
+    if(typeof mainData.description === 'undefined') {
+         descriptionText = ''
+    }else{
+         descriptionText = 'Описание: '+mainData.description
+    }
 
     return (
         <div className={'card-product'}>
-            <div className={'img_wrap'}>
-                <img src={mainData.img} className={'img-mini'} alt="Product Image"/>
+            <div style={{
+                background: '#454545', borderRadius: '10px', marginLeft: '10px',
+                marginRight: '10px',
+                width: String(window.innerWidth - 20) + 'px',
+                marginTop: '10px',
+                paddingBottom: '5px'
+            }}>
+                <img src={mainData.img} className={'img'} style={{
+                    height: String(window.innerWidth - 20) + 'px',
+                    borderTopLeftRadius: '10px',
+                    borderTopRightRadius: '10px'
+                }}
+                     alt="Product Image"/>
+                <div style={{
+                    color: 'white',
+                    fontSize: '22px',
+                    textAlign: 'center',
+                    fontFamily: "Argentum Sans VF Arial",
+                    marginTop: '7px',
+                    marginBottom: '7px'
+                }}>{mainData.title}</div>
+                <div style={{
+                    marginTop: '7px',
+                    fontSize: '14px',
+                    color: 'white',
+                    fontFamily: "Argentum Sans VF Arial"
+                }}>{descriptionText}
+                </div>
+                <div style={{
+                    marginTop: '7px',
+                    fontSize: '14px',
+                    color: 'white',
+                    fontFamily: "Argentum Sans VF Arial"
+                }}>{'Платформа: ' + mainData.platform}
+                </div>
+                <button className={'all-see-button'} onClick={buttonLink}
+                        style={{
+                            background: buttonColor,
+                            width: String(window.innerWidth - 20 - 8) + 'px',
+                            transitionProperty: 'background',
+                            transitionDuration: '0.2s',
+                        }}>{buttonText}
+                </button>
             </div>
-            <div className={'text-element name-card-element'}>{mainData.title}</div>
-            <button onClick={onSendData} className={'all-see-button'}>Добавить в корзину</button>
         </div>
     );
 };
--- src/components/UI/HomeBlockElement.jsx
+++ src/components/UI/HomeBlockElement.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const HomeBlockElement = ({path, data}) => {
+
+    let oldPrice = ''
+    if(typeof data.old_price === 'undefined') {
+        oldPrice = ''
+    }else{
+        oldPrice = String(data.old_price)+ ' ₽'
+    }
+
+    let dataRelease = ''
+    if(typeof data.release === 'undefined') {
+        dataRelease = ''
+    }else{
+        const val = data.release*1000000;
+        const msecsPerDay = 24 * 60 * 60 * 1000;
+        const ms = Math.round(val / 1E+6 * msecsPerDay);
+        const start = new Date(1900, 0, 1);
+        const date = new Date(ms + start.valueOf());
+        console.log(date.toString());
+    }
+
     return (
         <div className={'home-block-element'}>
             <Link to={'/home/' + path + '/' + data.id} className={'link-element'}>
                 <div className={'box-home-block-element'}>
                     <img src={data.img} alt={data.title} className={'img-home'}/>
                     <div className={'text-element name-element'}>{data.title}</div>
-                    <div className={'text-element price-element'}>{String(data.price) + ' ₽'}</div>
+                    <div style={{display: 'flex', justifyContent: 'left'}}>
+                        <div className={'text-element price-element'}>{String(data.price) + ' ₽'}</div>
+                        <div className={'text-element price-element'}
+                             style={{textDecoration: 'line-through', color: 'gray'}}>{oldPrice}</div>
+                        {/*<div className={'text-element price-element'}*/}
+                        {/*     style={{textDecoration: 'line-through', color: 'gray'}}>{dataRelease}</div>*/}
+                    </div>
                 </div>
             </Link>
         </div>

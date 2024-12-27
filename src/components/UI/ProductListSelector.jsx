ea789ed7fb6000d1a47a4fcacb236a098149cd45
43m04ka <89536403007r@gmail.com>
2024-12-27 20:13:34 +0300

    init

--- src/components/UI/ProductListSelector.jsx
+++ src/components/UI/ProductListSelector.jsx
@@ -25,116 +25,121 @@ const ProductListSelector = ({main_data, page, height}) => {
     })
     data = [...data, ...vsArray]
 
-    let cordCategory = (window.innerWidth - 7 -7 -2 -2 ) / data.length * selectCategory +1.5
-    let cordView = (window.innerWidth - 7 -7 -2 -2 ) / data[selectCategory].body.length * selectView +1.5
+    let cordCategory = (window.innerWidth - 7 - 7 - 2 - 2) / data.length * selectCategory + 1.5
+    let cordView = (window.innerWidth - 7 - 7 - 2 - 2) / data[selectCategory].body.length * selectView + 1.5
 
     let url = data[selectCategory].body[selectView].img
 
     console.log(data);
     return (
         <div>
-            <img src={url} className={'img_wrap'}/>
             <div style={{
-                display: 'grid',
-                alignItems: 'center',
+                background: '#565656', borderRadius: '7px', marginLeft: '7px',
+                marginRight: '7px',
                 width: String(window.innerWidth - 14) + 'px',
-                border: '1px gray solid',
-                borderRadius: '7px',
-                marginLeft: '7px',
-                marginBottom: '7px'
+                marginTop: '7px'
             }}>
+                <img src={url} className={'img_wrap'} style={{borderTopRightRadius:'7px', borderTopLeftRadius:'7px'}}/>
                 <div style={{
-                    background: '#414BE0FF',
-                    width: String(((window.innerWidth-7-7 -2-2) - (data.length-1)*2 )/data.length ) + 'px',
-                    height: '25px',
-                    marginTop: '2px',
-                    marginBottom: '2px',
-                    borderRadius: '5px',
-                    position: 'relative',
-                    marginLeft: String(cordCategory) + 'px',
-                    transitionProperty: 'margin-left',
-                    transitionDuration: '0.2s',
-                }}></div>
+                    display: 'grid',
+                    alignItems: 'center',
+                    width: String(window.innerWidth - 14) + 'px',
+                    border: '1px gray solid',
+                    borderRadius: '7px',
+                    marginBottom: '7px'
+                }}>
+                    <div style={{
+                        background: '#414BE0FF',
+                        width: String(((window.innerWidth - 7 - 7 - 2 - 2) - (data.length - 1) * 2) / data.length) + 'px',
+                        height: '25px',
+                        marginTop: '2px',
+                        marginBottom: '2px',
+                        borderRadius: '5px',
+                        position: 'relative',
+                        marginLeft: String(cordCategory) + 'px',
+                        transitionProperty: 'margin-left',
+                        transitionDuration: '0.2s',
+                    }}></div>
 
-                {data.map(el => (
-                    <div key={el.id} style={{
-                        position: 'absolute',
-                        marginLeft: String((el.id) * (window.innerWidth - 14) / data.length) + 'px',
-                        width: String((window.innerWidth - 24) / data.length) + 'px',
-                        height: '30px'
-                    }}>
-                        <div style={{
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            display: 'flex',
-                            height: '30px',
-                            overflow: 'hidden',
-                        }} onClick={() => {
-                            setSelectCategory(el.id)
-                            setSelectView(0)
-                        }}>
-                            <div>
-                                {el.title}
-                            </div>
-
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div style={{
-                display: 'grid',
-                alignItems: 'center',
-                width: String(window.innerWidth - 14) + 'px',
-                border: '1px gray solid',
-                borderRadius: '7px',
-                marginLeft: '7px',
-            }}>
-                <div style={{
-                    background: '#414BE0FF',
-                    width: String(((window.innerWidth-7-7 -2-2) - (data[selectCategory].body.length-1)*2 )/data[selectCategory].body.length ) + 'px',
-                    height: '55px',
-                    borderRadius: '5px',
-                    marginTop: '2px',
-                    marginBottom: '2px',
-                    marginRight: '2px',
-                    position: 'relative',
-                    marginLeft: String(cordView) + 'px',
-                    transitionProperty: 'margin-left',
-                    transitionDuration: '0.2s',
-                }}></div>
-                {data[selectCategory].body.map(el => (
-                    <div key={el.id} style={{
-                        position: 'absolute',
-                        marginLeft: String(((el.id - 1) % 3) * (window.innerWidth - 14) / data[selectCategory].body.length + 'px'),
-                        width: String((window.innerWidth - 24) / data[selectCategory].body.length) + 'px',
-                        height: '60px'
-                    }}>
-                        <div style={{
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            display: 'flex',
-                            height: '60px',
-                            overflow: 'hidden',
-                            width: String((window.innerWidth - 24) / data[selectCategory].body.length) + 'px',
-                        }} onClick={() => {
-                            setSelectView(((el.id - 1) % 3))
+                    {data.map(el => (
+                        <div key={el.id} style={{
+                            position: 'absolute',
+                            marginLeft: String((el.id) * (window.innerWidth - 14) / data.length) + 'px',
+                            width: String((window.innerWidth - 24) / data.length) + 'px',
+                            height: '30px'
                         }}>
                             <div style={{
-                                color: 'white',
-                                fontSize: '10px',
-                                height: '50px',
-                                display: 'grid',
-                                gridTemplateRows: '1fr 1fr',
-                                justifyItems: 'center',
-                                alignItems: 'center'
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                display: 'flex',
+                                height: '30px',
+                                overflow: 'hidden',
+                            }} onClick={() => {
+                                setSelectCategory(el.id)
+                                setSelectView(0)
                             }}>
-                                <div>{el.view}</div>
-                                <div>{el.price}</div>
+                                <div>
+                                    {el.title}
+                                </div>
+
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+                <div style={{
+                    display: 'grid',
+                    alignItems: 'center',
+                    width: String(window.innerWidth - 14) + 'px',
+                    border: '1px gray solid',
+                    borderRadius: '7px',
+                }}>
+                    <div style={{
+                        background: '#414BE0FF',
+                        width: String(((window.innerWidth - 7 - 7 - 2 - 2) - (data[selectCategory].body.length - 1) * 2) / data[selectCategory].body.length) + 'px',
+                        height: '55px',
+                        borderRadius: '5px',
+                        marginTop: '2px',
+                        marginBottom: '2px',
+                        marginRight: '2px',
+                        position: 'relative',
+                        marginLeft: String(cordView) + 'px',
+                        transitionProperty: 'margin-left',
+                        transitionDuration: '0.2s',
+                    }}></div>
+                    {data[selectCategory].body.map(el => (
+                        <div key={el.id} style={{
+                            position: 'absolute',
+                            marginLeft: String(((el.id - 1) % 3) * (window.innerWidth - 14) / data[selectCategory].body.length + 'px'),
+                            width: String((window.innerWidth - 24) / data[selectCategory].body.length) + 'px',
+                            height: '60px'
+                        }}>
+                            <div style={{
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                display: 'flex',
+                                height: '60px',
+                                overflow: 'hidden',
+                                width: String((window.innerWidth - 24) / data[selectCategory].body.length) + 'px',
+                            }} onClick={() => {
+                                setSelectView(((el.id - 1) % 3))
+                            }}>
+                                <div style={{
+                                    color: 'white',
+                                    fontSize: '10px',
+                                    height: '50px',
+                                    display: 'grid',
+                                    gridTemplateRows: '1fr 1fr',
+                                    justifyItems: 'center',
+                                    alignItems: 'center'
+                                }}>
+                                    <div>{el.view}</div>
+                                    <div>{el.price}</div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
 
+                </div>
             </div>
         </div>
     );

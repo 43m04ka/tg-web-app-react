60a74c32180573c8813764d41197c374499c90f9
43m04ka <89536403007r@gmail.com>
2025-01-02 19:28:02 +0300

    init

--- src/components/UI/HomeBlockElement.jsx
+++ src/components/UI/HomeBlockElement.jsx
@@ -4,17 +4,17 @@ import {Link} from "react-router-dom";
 const HomeBlockElement = ({path, data}) => {
 
     let oldPrice = ''
-    if(typeof data.old_price === 'undefined') {
+    if (typeof data.old_price === 'undefined') {
         oldPrice = ''
-    }else{
-        oldPrice = String(data.old_price)+ ' ₽'
+    } else {
+        oldPrice = String(data.old_price) + ' ₽'
     }
 
     let dataRelease = ''
-    if(typeof data.release === 'undefined') {
+    if (typeof data.release === 'undefined') {
         dataRelease = ''
-    }else{
-        const val = data.release*1000000;
+    } else {
+        const val = data.release * 1000000;
         const msecsPerDay = 24 * 60 * 60 * 1000;
         const ms = Math.round(val / 1E+6 * msecsPerDay);
         const start = new Date(1900, 0, 1);
@@ -26,16 +26,38 @@ const HomeBlockElement = ({path, data}) => {
         <div className={'home-block-element'}>
             <Link to={'/home/' + path + '/' + data.id} className={'link-element'}>
                 <div className={'box-home-block-element'}>
-                    <div style={{backgroundImage:'url("'+data.img+'+")', backgroundRepeat:'no-repeat', backgroundSize:'cover', alignContent:'end', justifyItems:'left'}} className={'img-home'}>
-                        <div className={'text-element'} style={{lineHeight:'20px', background:'#191919', paddingLeft:'3px', paddingRight:'3px', borderRadius:'5px', marginBottom:'7px'}}>{data.platform}</div>
+                    <div style={{
+                        backgroundImage: 'url("' + data.img + '+")',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover',
+                        alignContent: 'end',
+                        justifyItems: 'left'
+                    }} className={'img-home'}>
+                        <div style={{
+                            lineHeight: '20px',
+                            background: '#191919',
+                            paddingLeft: '3px',
+                            paddingRight: '3px',
+                            borderRadius: '5px',
+                            marginBottom: '5px',
+                            textDecoration: 'none',
+                            textAlign:'left',
+                            marginLeft:'5px',
+                            fontFamily:"'Montserrat', sans-serif",
+                            fontWeight:700,
+                            fontSize:'12px',
+                            overflow:'hidden',
+                            color:'white'
+                        }}>{data.platform}</div>
                     </div>
-                    <div style={{height:'37px', overflow:'hidden'}}>
+                    <div style={{height: '37px', overflow: 'hidden'}}>
                         <div className={'text-element name-element'}>{data.title}</div>
                     </div>
                     <div style={{display: 'flex', justifyContent: 'left'}}>
-                        <div className={'text-element price-element'} style={{fontSize:'14px'}}>{String(data.price) + ' ₽'}</div>
                         <div className={'text-element price-element'}
-                             style={{textDecoration: 'line-through', color: 'gray', fontSize:'14px'}}>{oldPrice}</div>
+                             style={{fontSize: '14px'}}>{String(data.price) + ' ₽'}</div>
+                        <div className={'text-element price-element'}
+                             style={{textDecoration: 'line-through', color: 'gray', fontSize: '14px'}}>{oldPrice}</div>
                         {/*<div className={'text-element price-element'}*/}
                         {/*     style={{textDecoration: 'line-through', color: 'gray'}}>{dataRelease}</div>*/}
                     </div>

f373b6921be504535c236c2860e27fcb67b183d8
43m04ka <89536403007r@gmail.com>
2025-01-05 00:58:30 +0300

    init

--- src/components/UI/Basket.jsx
+++ src/components/UI/Basket.jsx
@@ -17,7 +17,7 @@ const Basket = ({height}) => {
 
     const sendDataProduct = {
         method: 'buy',
-        user: {id: 5106439090},
+        user: user,
     }
 
     function rgb([r, g, b]) {
@@ -38,7 +38,7 @@ const Basket = ({height}) => {
 
     const sendData = {
         method: 'get',
-        user: {id: 5106439090},
+        user: user,
     }
 
     const onGetData = useCallback(() => {

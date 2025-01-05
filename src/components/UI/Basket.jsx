1fbb8629a46e734b35e3ac05e912660e80c64f72
43m04ka <89536403007r@gmail.com>
2025-01-05 11:47:42 +0300

    init

--- src/components/UI/Basket.jsx
+++ src/components/UI/Basket.jsx
@@ -7,13 +7,13 @@ import {useTelegram} from "../../hooks/useTelegram";
 const Basket = ({height}) => {
     const {tg, user} = useTelegram();
     const navigate = useNavigate();
-    console.log(user)
 
     const [basket, setBasket] = useState([])
     const [myAcc, setMyAcc] = useState(0);
     const [colorYes, setColorYes] = useState([81, 164, 86]);
     const [colorNo, setColorNo] = useState([45, 12, 12]);
     const [status, setStatus] = useState(0);
+    const [buttonText, setButtonText] = React.useState('Оформить закказ и оплатить');
 
     const sendDataProduct = {
         method: 'buy',
@@ -25,6 +25,7 @@ const Basket = ({height}) => {
     }
 
     const onClickButton = useCallback(() => {
+        setButtonText('Оформляем заказ...')
         fetch('https://2ae04a56-b56e-4cc1-b14a-e7bf1761ebd5.selcdn.net/basket', {
             method: 'POST',
             headers: {
@@ -32,13 +33,21 @@ const Basket = ({height}) => {
             },
             body: JSON.stringify(sendDataProduct)
         }).then(r => {
-            console.log(r)
+            let Promise = r.json()
+            Promise.then(r => {
+                if (r.body) {
+                    setStatus(2);
+                } else {
+                    setStatus(0)
+                    setButtonText('Оформить заказ и оплатить')
+                }
+            })
         })
     }, [sendDataProduct])
 
     const sendData = {
         method: 'get',
-        user: {id: 5106439090},
+        user: user,
     }
 
     const onGetData = useCallback(() => {
@@ -52,7 +61,7 @@ const Basket = ({height}) => {
             let Promise = r.json()
             Promise.then(r => {
                 setBasket(r.body);
-                setStatus(1);
+                setStatus(1)
             })
         })
     }, [sendData])
@@ -90,8 +99,9 @@ const Basket = ({height}) => {
     }
 
 
-    const styleYes = {background: rgb(colorYes), height:'37px'}
-    const styleNo = {background: rgb(colorNo), height:'37px'}
+    const styleYes = {background: rgb(colorYes), height: '37px'}
+    const styleNo = {background: rgb(colorNo), height: '37px'}
+
 
     if (status === 0) {
         onGetData()
@@ -100,7 +110,7 @@ const Basket = ({height}) => {
             marginTop: String(height / 2 - 60) + 'px',
             marginLeft: String(window.innerWidth / 2 - 40) + 'px'
         }}>Ожидайте</div>);
-    } else if (basket.length === 0) {
+    } else if (basket.length === 0 && status === 1) {
         return (
             <div style={{display: 'grid'}}>
                 <div style={{
@@ -131,7 +141,7 @@ const Basket = ({height}) => {
                     </Link>
                 </div>
             </div>)
-    } else {
+    } else if (status === 1) {
         let count = 1;
         basket.map(el => {
             el.number = count;
@@ -162,7 +172,7 @@ const Basket = ({height}) => {
                                 alignItems: 'center',
                                 justifyContent: 'space-between',
                                 marginTop: '5px',
-                                height:'40px'
+                                height: '40px'
                             }}>
                                 <div className={"text-element"}
                                      style={{fontSize: '15px', marginLeft: '0', marginRight: '0',}}>У
@@ -192,7 +202,7 @@ const Basket = ({height}) => {
                                 alignItems: 'center',
                                 justifyContent: 'space-between',
                                 marginTop: '5px',
-                                height:'40px'
+                                height: '40px'
                             }}>
                                 <div className={"text-element"}
                                      style={{fontSize: '15px', marginLeft: '0', marginRight: '0',}}>У
@@ -201,7 +211,17 @@ const Basket = ({height}) => {
                                 <div style={{
                                     width: '140px',
                                 }}>
-                                    <input style={{height:'37px', width:'140px', borderRadius:'10px', background:'#454545', border:'0px', fontSize:'15px', color:'white', fontFamily:"'Montserrat', sans-serif", paddingLeft:'5px'}} />
+                                    <input style={{
+                                        height: '37px',
+                                        width: '140px',
+                                        borderRadius: '10px',
+                                        background: '#454545',
+                                        border: '0px',
+                                        fontSize: '15px',
+                                        color: 'white',
+                                        fontFamily: "'Montserrat', sans-serif",
+                                        paddingLeft: '5px'
+                                    }}/>
                                 </div>
                             </div>
                             <div style={{
@@ -209,7 +229,7 @@ const Basket = ({height}) => {
                                 flexDirection: 'row',
                                 justifyContent: 'space-between',
                                 marginTop: '5px',
-                                height:'40px'
+                                height: '40px'
                             }}>
                                 <div style={{marginTop: '10px', fontSize: '15px', marginLeft: '0', marginRight: '0'}}
                                      className={'text-element'}>Итого:
@@ -219,18 +239,68 @@ const Basket = ({height}) => {
                                 </div>
                             </div>
                         </div>
-                        <button className={'all-see-button'} style={{marginTop: '10px', width:String(window.innerWidth-30)+'px', background:'#52a557'}}
-                                onClick={onClickButton}>Оформить
-                            заказ
+                        <button className={'all-see-button'} style={{
+                            marginTop: '10px',
+                            width: String(window.innerWidth - 30) + 'px',
+                            background: '#52a557'
+                        }}
+                                onClick={onClickButton}>{buttonText}
                         </button>
                     </div>
                 </div>
             </div>
 
         );
+    } else if (status === 2) {
+        return (<div style={{flexDirection: 'column', display: 'flex'}}>
+            <div className={'text-element'} style={{
+                marginTop: '200px',
+                width: String(window.innerWidth) + 'px',
+                textAlign: 'center',
+                fontSize: '20px',
+                marginLeft: '0px'
+            }}>Заказ
+                успешно оформлен, спасибо!
+            </div>
+            <div style={{
+                justifyItems: 'center', width: String(window.innerWidth - 150) + 'px',
+                marginLeft: '75px', marginTop: '30px', marginBottom: '30px'
+            }}>
+                <div className={'background-heart'} style={{height: '60px', width: '60px'}}/>
+            </div>
+            <div className={'text-element'} style={{
+                width: String(window.innerWidth - 90) + 'px',
+                textAlign: 'center',
+                fontSize: '20px',
+                marginLeft: '45px'
+            }}>Совсем скоро с Вами свяжется наш менеджер - "gwstore_admin для оплаты и вктивации Вашего заказа.
+            </div>
+            <a className={'link-element text-element'}
+               href={'https://t.me/gwstore_admin'}>
+                <button className={'all-see-button'} style={{
+                    marginTop: '15px',
+                    height: '50px',
+                    width: String(window.innerWidth - 50) + 'px',
+                    background: '#52a557'
+                }}
+                        onClick={onClickButton}>Написать менеджеру
+                </button></a>
+                <Link to={'/home'} className={"link-element"}>
+                <button className={'all-see-button'} style={{
+                    marginTop: '10px',
+                    marginLeft: '25px',
+                    height: '50px',
+                    width: String(window.innerWidth - 50) + 'px',
+                    background: '#454545'
+                }}
+                        onClick={onClickButton}>Вернутся на главную
+                </button>
+        </Link>
+        </div>
+    )
     }
 
 
-};
+    };
 
-export default Basket;
+    export default Basket;

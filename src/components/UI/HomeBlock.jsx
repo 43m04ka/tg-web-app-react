080f01383a4b453940aa12c8273413e7736ba285
43m04ka <89536403007r@gmail.com>
2025-01-25 18:39:39 +0300

    init

--- src/components/UI/HomeBlock.jsx
+++ src/components/UI/HomeBlock.jsx
@@ -17,16 +17,15 @@ const HomeBlock = ({data}) => {
                     )
                 )}
                 <div className={'box-home-block-element home-block-element'}>
-                    <div style={{marginLeft:'2px', marginRight:'7px'}}>
-                        <Link to={'/home/' + data.path} className={'link-element'}>
-                            <button className={'all-see-button'} style={{
-                                height: '37px',
-                                width: '150px',
+                    <div style={{marginLeft:'2px', marginRight:'7px',border:'0px solid #454545',
+                        borderRadius:'7px', padding:'5px'}}>
+                        <Link to={'/home/' + data.path} className={'link-element'} >
+                            <div className={'background-arrow'} style={{
+                                height: '25px',
+                                width: '25px',
                                 marginTop: '0',
                                 fontSize:'12.5px',
-                                border:'2px solid #454545', background:'#171717'
-                            }}>Смотреть больше
-                            </button>
+                            }}/>
                         </Link>
                     </div>
                 </div>

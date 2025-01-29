340d950e905846e82143405fdd810fe63a40c17b
43m04ka <89536403007r@gmail.com>
2025-01-29 20:00:32 +0300

    init

--- src/components/UI/HeadSelector.jsx
+++ src/components/UI/HeadSelector.jsx
@@ -93,7 +93,7 @@ const HeadSelector = ({hidden, basketData, page}) => {
                             color: 'black',
                             fontFamily: "'Montserrat', sans-serif",
                             fontVariant: 'small-caps'
-                        }}>Найти игру, подписку...
+                        }}>Найти игру, подписку, смысл жизни...
                         </div>
                     </div>
                 </Link>

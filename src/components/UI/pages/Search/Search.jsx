1b3269a36e36c82ffbea8aef64f5ebcad8f1411d
43m04ka <899536403007r@gmail.com>
2025-10-26 22:17:28 +0300

    init

--- src/components/UI/pages/Search/Search.jsx
+++ src/components/UI/pages/Search/Search.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import style from './Search.module.scss'
+import {useTelegram} from "../../../../hooks/useTelegram";
 
 const Search = () => {
+    const {tg} = useTelegram()
+    console.log(tg.viewportHeight)
+
     return (
         <div className={style['mainDivision']}>
             <div>
                 <div>
                     <div/>
-                    <input placeholder={'Поиск'}></input>
+                    <input placeholder={'Поиск'} value={tg.viewportHeight}></input>
                 </div>
             </div>
         </div>

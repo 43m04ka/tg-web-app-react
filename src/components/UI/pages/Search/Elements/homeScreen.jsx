85069c8978d6048db6e885e8fa3f0cd7b4d5c5d1
43m04ka <899536403007r@gmail.com>
2025-10-27 19:31:46 +0300

    init

--- src/components/UI/pages/Search/Elements/homeScreen.jsx
+++ src/components/UI/pages/Search/Elements/homeScreen.jsx
@@ -15,7 +15,8 @@ const HomeScreen = ({setInputValue}) => {
     return (
         <div className={style['homeScreen']}>
             {clueList.map((item, index) => (
-                <div onClick={()=>{setInputValue(item.name)}}>{item.name}</div>
+                <div className={style['animClue']} style={{webkitAnimationDelay: String(0.1 * index) + 's',
+                    animationDelay: String(0.1 * index + 0.2) +  's'}} onClick={()=>{setInputValue(item.name)}}>{item.name}</div>
             ))}
         </div>
     );

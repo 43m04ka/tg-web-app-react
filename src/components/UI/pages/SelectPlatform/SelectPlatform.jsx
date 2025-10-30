091b814e6a730a4886f1726665005cc3a3527c4b
43m04ka <899536403007r@gmail.com>
2025-10-30 21:19:52 +0300

    init

--- src/components/UI/pages/SelectPlatform/SelectPlatform.jsx
+++ src/components/UI/pages/SelectPlatform/SelectPlatform.jsx
@@ -4,16 +4,22 @@ import useGlobalData from "../../../../hooks/useGlobalData";
 
 const SelectPlatform = () => {
     const {pageList, setPageId} = useGlobalData()
+    const [mouseDownId, setMouseDownId] = React.useState(-1);
 
     return (
-        <div className={style['container']}>
+        <div className={style['container']} onTouchMove={(e) => {
+            let id = document.elementFromPoint(e.changedTouches[0].pageX, e.changedTouches[0].pageY).id || -1
+            if (id !== mouseDownId) {
+                setMouseDownId(Number(id))
+            }
+        }}>
             {pageList.map((item, index) => (
                 <>
-                    <div onClick={() => setPageId(item.id)}>
-                        <div>{item.name}</div>
-                        <div style={{backgroundImage: `url(${item.url})`}}/>
+                    <div className={style[mouseDownId === index ? 'selectedItem' : '']} id={index}>
+                        <div id={index}>{item.name}</div>
+                        <div id={index} style={{backgroundImage: `url(${item.url})`}}/>
                     </div>
-                    { index + 1 !== pageList.length ? <div/> : ''}
+                    {index + 1 !== pageList.length ? <div/> : ''}
                 </>
             ))}
         </div>

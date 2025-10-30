55551f8e3c8d64c72cbe8e03cc98d43e0c70d740
43m04ka <899536403007r@gmail.com>
2025-10-31 01:31:17 +0300

    init

--- src/components/UI/pages/SelectPlatform/SelectPlatform.jsx
+++ src/components/UI/pages/SelectPlatform/SelectPlatform.jsx
@@ -1,11 +1,14 @@
+
 import React from 'react';
 import style from './SelectPlatform.module.scss'
 import useGlobalData from "../../../../hooks/useGlobalData";
+import {useNavigate} from "react-router-dom";
 
 const SelectPlatform = ({onClose}) => {
     const {pageList, setPageId} = useGlobalData()
     const [mouseDownId, setMouseDownId] = React.useState(-1);
     const [isOpen, setIsOpen] = React.useState(false)
+    const navigate = useNavigate();
 
     return (
         <div className={style['container']} id={-2}
@@ -29,12 +32,13 @@ const SelectPlatform = ({onClose}) => {
             let id = Number(document.elementFromPoint(e.changedTouches[0].pageX, e.changedTouches[0].pageY).id)
             if ((typeof id !== 'undefined') && id !== -1 && id !== -2) {
                 setPageId(pageList[id].id)
+                navigate('/' + pageList[id].link)
             }
             if (id !== -2 || (id === -2 && isOpen && (mouseDownId !== -2))) {
-                setTimeout(() => setIsOpen(false), 50)
+                setIsOpen(false)
             }
         }}>
-            <div style={!isOpen ? {scale: '0.9', opacity:'0'} : {height: 'max-content', opacity:'1', scale:'1'}}>
+            <div style={!isOpen ? {scale: '0', opacity:'0', height:'0'} : {height: String((7 * (pageList.length + 2) + 4)/100 * window.innerWidth) + 'px', opacity:'1', scale:'1'}}>
                 <div className={style['label']}>Выберите платформу</div>
                 {pageList.map((item, index) => (
                     <>

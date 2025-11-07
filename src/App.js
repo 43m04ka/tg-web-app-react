0785b9f76033b22fe9988b7ee6246faeba70c717
43m04ka <899536403007r@gmail.com>
2025-11-07 13:43:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ import {Route, Routes, useNavigate} from "react-router-dom";
 import ProductList from "./components/UI/pages/other/ProductList";
 import MainPage from "./components/UI/MainPage";
 import ErrorPage from "./components/UI/pages/other/ErrorPage";
-import Search_old from "./components/UI/pages/Search/Search_old";
-import Basket_old from "./components/UI/pages/Basket/Bascet_old/Basket_old";
 import AdminPanel from "./components/UI/pages/AdminPanel/AdminPanel";
-import Info from "./components/UI/pages/other/Info";
 import ProductListSelector from "./components/UI/pages/other/ProductListSelector";
 import History from "./components/UI/pages/other/History";
 import Favorites from "./components/UI/pages/other/Favorites";
-import Roulette from "./components/UI/pages/other/Roulette";
 import AP_Authentication from "./components/UI/pages/AdminPanel/AP_Authentication";
 import useGlobalData from "./hooks/useGlobalData";
 import Product from "./components/UI/pages/Product/Product";
+import style from './App.module.scss'
 
 
 function App() {
@@ -40,6 +37,7 @@ function App() {
 
 
     const [size, setSize] = React.useState(window.innerHeight);
+    const [isLoaded, setIsLoaded] = React.useState(true);
 
     const resizeHandler = () => {
         setSize(window.innerHeight);
@@ -71,31 +69,42 @@ function App() {
         updatePreviewFavoriteData(user.id)
     }, [])
 
-    if (pageList !== null) {
+    if (!isLoaded) {
         if (window.location.pathname === '/') {
             navigate(pageList[0]['link'])
         }
-        return (
-            <div className="App">
-                <Routes>
-                    {pageList.map((page) => (
-                        <Route path={page['link'] + '/*'} key={page['id']} element={<MainPage page={page}/>}/>))}
-                    <Route path={'favorites'} element={<Favorites/>}/>
-                    <Route path={'/catalog/*'} element={<ProductList height={size}/>}/>
-                    <Route path={'/card/*'} element={<Product/>}/>
-                    <Route path={'/choice-catalog/*'} element={<ProductListSelector/>}/>
-                    <Route path={'admin-panel/*'} element={<AdminPanel/>}/>
-                    <Route path={'admin'} element={<AP_Authentication/>}/>
-                    <Route path={'/history'} element={<History/>}/>
-                    <Route path="*" element={<ErrorPage/>}/>
-                </Routes>
-            </div>
-        );
+        return (<div className="App">
+            <Routes>
+                {pageList.map((page) => (
+                    <Route path={page['link'] + '/*'} key={page['id']} element={<MainPage page={page}/>}/>))}
+                <Route path={'favorites'} element={<Favorites/>}/>
+                <Route path={'/catalog/*'} element={<ProductList height={size}/>}/>
+                <Route path={'/card/*'} element={<Product/>}/>
+                <Route path={'/choice-catalog/*'} element={<ProductListSelector/>}/>
+                <Route path={'admin-panel/*'} element={<AdminPanel/>}/>
+                <Route path={'admin'} element={<AP_Authentication/>}/>
+                <Route path={'/history'} element={<History/>}/>
+                <Route path="*" element={<ErrorPage/>}/>
+            </Routes>
+        </div>);
     } else {
-        return (<div className={'plup-loader'} style={{
-            marginTop: String(size / 2 - 50) + 'px',
-            marginLeft: String(window.innerWidth / 2 - 50) + 'px'
-        }}></div>);
+        return (<div className={style["container"]}>
+            <div className={style['loader']}>
+                <div className={style['box']}>
+                    <div className={style['logo']}>
+                        <div/>
+                    </div>
+                </div>
+                <div className={style['box']}></div>
+                <div className={style['box']}></div>
+                <div className={style['box']}></div>
+                <div className={style['box']}></div>
+            </div>
+            <div className={style['fillContainer']}>
+                <div style={{scale: (catalogList !== null && pageList !== null && mainPageCards !== null ? '13' : '0')}}/>
+                {catalogList !== null && pageList !== null && mainPageCards !== null ? setTimeout(()=>setIsLoaded(false), 200) : ''}
+            </div>
+        </div>);
     }
 }
 

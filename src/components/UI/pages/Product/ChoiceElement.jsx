00e9c9313deeb45d4e9ae242f922162093956bdb
43m04ka <899536403007r@gmail.com>
2025-11-22 03:05:35 +0300

    init

--- src/components/UI/pages/Product/ChoiceElement.jsx
+++ src/components/UI/pages/Product/ChoiceElement.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import style from './Product.module.scss';
 
-const ChoiceElement = ({set, list, index : selectedIndex, parameter}) => {
-    return (<div className={style['choiceBlock']}>
-            {list.map((item, index) => (<div style={{background: selectedIndex === index ? '#404ADE' : '#838383'}} onClick={() => {
+
+const ChoiceElement = ({set, list, index: selectedIndex, parameter}) => {
+    if (list.length === 3) {
+        return (
+            <div className={style['subSlider']}>
+                <div>
+                    {list.map((item, index) => (
+                        <div onClick={() => {
+                            set(index)
+                        }}>{item[parameter]}</div>))}
+                </div>
+                <div className={style['v' + String(selectedIndex+1)]}/>
+            </div>
+        );
+    } else {
+        return (<div className={style['choiceBlock']}>
+            {list.map((item, index) => (
+                <div style={{background: selectedIndex === index ? '#404ADE' : '#838383'}} onClick={() => {
                     set(index)
                 }}>{item[parameter]}</div>))}
         </div>);
+    }
 };
 
 export default ChoiceElement;

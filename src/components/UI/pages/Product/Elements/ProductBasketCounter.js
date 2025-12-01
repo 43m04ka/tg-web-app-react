1432ce0c2e20b356113d0376d425e07c9302e1ed
43m04ka <899536403007r@gmail.com>
2025-12-01 16:52:37 +0300

    init

--- src/components/UI/pages/Product/Elements/ProductBasketCounter.js
+++ src/components/UI/pages/Product/Elements/ProductBasketCounter.js
@@ -2,12 +2,15 @@ import React from 'react';
 import style from '../Product.module.scss'
 
 const ProductBasketCounter = () => {
+
+    const [counter, setCounter] = React.useState(1);
+
     return (<div className={style['productBasketCounter']}>
-        <div>
+        <div onClick={()=>{setCounter(counter-1)}}>
             <p>-</p>
         </div>
-        <p>1 шт.</p>
-        <div>
+        <p>{counter} шт.</p>
+        <div onClick={()=>{setCounter(counter+1)}}>
             <p>+</p>
         </div>
     </div>);

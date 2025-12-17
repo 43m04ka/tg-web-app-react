5a69b32cc4bc343351d7118d11f1a759c8523a15
43m04ka <899536403007r@gmail.com>
2025-12-17 22:15:20 +0300

    init

--- src/components/UI/pages/Catalog/Catalog.jsx
+++ src/components/UI/pages/Catalog/Catalog.jsx
@@ -16,6 +16,7 @@ let lastCardList = null
 let lastPath = ''
 let len = 1
 let onLoad = false
+let lastJson = {sorting: 'default', platform: [], language: [], numberPlayers: []}
 
 const Catalog = () => {
 
@@ -27,7 +28,7 @@ const Catalog = () => {
 
     const [sortWindowOpen, setSortWindowOpen] = useState(false);
     const [filterWindowOpen, setFilterWindowOpen] = useState(false);
-    const [json, setJson] = useState({sorting: 'default', platform: [], language: [], numberPlayers: []});
+    const [json, setJson] = useState(lastJson);
 
     const [icon, setIcon] = useState('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAACjUlEQVR4nO3dR24UQRSH8bciCAEmR2PSFRBwJ0AI7mLBCZAQG4JkjmDA5OBAnDmDkYDNh0qalkwS4GlPvffq/5N65U2Xv03PTNVrMxEREREREREREZkI4DxwdXSdq30/zQJ2Avf41d3yt9r312KMef7sMbC79n02AZgCHvB3T4A9te83Nf49hqJMKMZD/t9TRekZsAt4tI4Ya6Psrb2OFBg/RueZoowJ2Ac8pz9vgIO11xU5xoseYyjKegH7NyhGZxE4VHudkWK83MAYiuIwRmcJOFx73S4BB4BXTJ6iOIrRWVaUH2O8pr5l4Ii1rDx+OonRWWk2CnAUeIs/H4Hj1hJg2mmM9qKMYrzDv0/ACcsMOBYkRv4ooxjviWcAnLRMAsdYG+WUZQDMAB+Ibxg+SqIY8aOUx8bR42M2Q+C0BfzWttx4VsOyRosCuEV+Ny0CYAfwjfy+AtvNu/LMTjtmzDtgE7BKfp+BzRYBcI38Zi3xHtxo5sMdeQC2AheBOWAhyTUHXAC21P7/ioiIiIiIRPlgeBm47+AD3UJPV1nLpbI2a/RcoFfla6EpiwK4Tn6zFkFDX7+vlrWad2WnH+2YNu/Kz5qjnzez+wJsswjKBgDyu2FRNLANaFDO0VskCXctpti9mC3KMGyMRDvfU+6Ajx5lkPWMSKTTU02coopyvjB/jEAncJs8iev1jHp7MX6a4lCGiXmz0vI0B43W8EbDZxxyMJ5pSeOZ6k+T6yiGoyiLmrtYfyJpRzEczOztaHavg6nWHcUYc3/XAv3R/PdxoWH8qd4d0tHrKvqGXujiD3rlUcg3tHX0prZJQa/Ncxvlzm9i3A43ZSET4CxwZXSdqX0/IiIiIiIiIiIi1ojv9aD4fOfUvPQAAAAASUVORK5CYII=')
 
@@ -40,6 +41,7 @@ const Catalog = () => {
         listNumber = number
         len = data.len
         onLoad = false;
+        lastJson = json
     }
 
     useEffect(() => {
@@ -58,6 +60,7 @@ const Catalog = () => {
                 setCardList(null)
                 lastScroll = 0
                 lastCardList = null
+                lastJson = {sorting: 'default', platform: [], language: [], numberPlayers: []}
             }
             return catalog
         } else {
@@ -128,9 +131,10 @@ const Catalog = () => {
                             <p>Фильтры</p>
                         </button>
                         {cardList.map(item => (
-                            <div style={{marginLeft: '6vw'}}>
-                                <CatalogItem key={item.id} product={item}/></div>))}
+                                <div style={{marginLeft: '6vw'}}>
+                                    <CatalogItem key={item.id} product={item}/></div>))}
                     </div>
+                    {cardList.length !== 0 ? '' : <div className={style['title']} style={{margin:'5vw', textAlign:'center'}}>Ничего не найдено, попробуйте изменить фильтры</div>}
                 </div>
                 {sortWindowOpen ? (<Sorting onClose={onClose} json={json} setJson={setJson} setIcon={setIcon}/>) : ''}
                 {filterWindowOpen ? (<Filter onClose={onClose} json={json} setJson={setJson}/>) : ''}

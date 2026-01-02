73a096652b64d0202a7bf3d955e58024d6885b80
43m04ka <899536403007r@gmail.com>
2026-01-02 20:04:33 +0300

    init

--- src/components/UI/pages/Basket/Elements/OrderPage.jsx
+++ src/components/UI/pages/Basket/Elements/OrderPage.jsx
@@ -6,24 +6,29 @@ const OrderPage = ({orderId}) => {
     const [stage, setStage] = useState(0);
 
     useEffect(() => {
-        setTimeout(()=>{
+        setTimeout(() => {
             setStage(1)
-        }, 5400)
+        }, 1550)
     }, [])
 
     return (
         <div className={style['container']}>
             {stage === 0 ?
-                (<div className={style['golo']}>
-                    <div/>
-                </div>)
+                (<>
+                    <div className={style['golo']}>
+                        <div/>
+                    </div>
+                    <div className={style['orderPageContainer']}/>
+                </>)
                 : ''}
+
 
             {/*<div style={{flexDirection: 'column', display: 'flex'}}>*/}
             {/*    <div className={style['happyDuck']}/>*/}
             {/*    <div className={style['label']}>{'Заказ №' + String(orderId) + ' успешно оформлен.'}</div>*/}
             {/*    <div className={style['label']} style={{marginBottom: '5vw'}}>*/}
-            {/*        Совсем скоро с Вами свяжется наш менеджер - @gwstore_admin для оплаты и активации Вашего заказа.*/}
+            {/*        Совсем скоро с Вами свяжется наш менеджер - @gwstore_admin для оплаты и активации Вашего*/}
+            {/*        заказа.*/}
             {/*    </div>*/}
             {/*    <a className={style['linkElement']}*/}
             {/*       href={'https://t.me/gwstore_admin'}>*/}
@@ -37,6 +42,7 @@ const OrderPage = ({orderId}) => {
             {/*        </button>*/}
             {/*    </Link>*/}
             {/*</div>*/}
+
         </div>
     );
 };
